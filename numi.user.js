6bc9bb3e27ee15cfef21ec899f170c6a1c780821
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2022-04-04 00:03:13 -0700

    Update and rename tampermonkey.js to numi.user.js

--- numi.user.js
+++ numi.user.js
@@ -1,15 +1,4 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
-
-// ==UserScript==
 // @name         Numi Template System
 // @namespace    http://tampermonkey.net/
 // @version      0.5
