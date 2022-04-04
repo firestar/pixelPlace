3faecb6234626b5d8ec63a16e77d7214718ecc1f
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2022-04-03 22:17:37 -0700

    Update template.js

--- template.js
+++ template.js
@@ -1,20 +1,21 @@
 const objects={
   'numi': {
     y: 1322,
+    x: 974,
     data: [
-        ['1974:1:black', '12:burgundy'],
-        ['1974:1:black', '12:burgundy'],
-        ['1974:1:black', '2:burgundy', '2:black', '3:burgundy', '2:black', '3:burgundy' ],
-        ['1974:1:black', '2:burgundy', '1:black', '1:dark_red', '3:black', '1:dark_red', '1:black', '3:burgundy'],
-        ['1974:1:black', '2:burgundy', '2:black', '3:dark_gray', '2:black', '3:burgundy' ],
-        ['1974:1:black', '1:burgundy', '2:black', '1:dark_gray', '1:black', '1:dark_gray', '1:black', '1:dark_gray', '2:black', '2:burgundy' ],
-        ['1974:1:black', '1:burgundy', '1:black', '7:dark_gray', '1:black', '2:burgundy' ],
-        ['1974:2:burgundy', '1:black', '1:dark_gray', '1:light_pink', '3:dark_gray', '1:light_pink', '1:dark_gray', '1:black', '2:burgundy' ],
-        ['1974:2:burgundy', '1:black', '7:dark_gray', '1:black', '2:burgundy' ],
-        ['1974:2:burgundy', '1:black', '7:dark_gray', '1:black', '2:burgundy' ],
-        ['1974:1:dark_brown', '1:burgundy', '9:black', '2:burgundy' ],
-        ['1974:1:brown', '1:dark_brown', '11:burgundy'],
-        ['1974:2:brown', '1:dark_brown', '10:burgundy'],
+        ['1:black', '12:burgundy'],
+        ['1:black', '12:burgundy'],
+        ['1:black', '2:burgundy', '2:black', '3:burgundy', '2:black', '3:burgundy' ],
+        ['1:black', '2:burgundy', '1:black', '1:dark_red', '3:black', '1:dark_red', '1:black', '3:burgundy'],
+        ['1:black', '2:burgundy', '2:black', '3:dark_gray', '2:black', '3:burgundy' ],
+        ['1:black', '1:burgundy', '2:black', '1:dark_gray', '1:black', '1:dark_gray', '1:black', '1:dark_gray', '2:black', '2:burgundy' ],
+        ['1:black', '1:burgundy', '1:black', '7:dark_gray', '1:black', '2:burgundy' ],
+        ['2:burgundy', '1:black', '1:dark_gray', '1:light_pink', '3:dark_gray', '1:light_pink', '1:dark_gray', '1:black', '2:burgundy' ],
+        ['2:burgundy', '1:black', '7:dark_gray', '1:black', '2:burgundy' ],
+        ['2:burgundy', '1:black', '7:dark_gray', '1:black', '2:burgundy' ],
+        ['1:dark_brown', '1:burgundy', '9:black', '2:burgundy' ],
+        ['1:brown', '1:dark_brown', '11:burgundy'],
+        ['2:brown', '1:dark_brown', '10:burgundy'],
     ]
   }
 }
